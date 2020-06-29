1c20c510fd14467959e6f4debf5a8c55e2025fe6
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-06-29 14:47:08 +1200

    Testing port change

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 
 export default {
-  server: { // Experimental
-    port: 3002, // default: 3000
-    host: '0.0.0.0' // default: localhost
-  },
   /*
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
