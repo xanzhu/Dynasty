d3f764dc0004c910acfba50dd0c7e9bda7247566
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-07-09 22:02:32 +1200

    House keeping

--- components/xanzhu-header-logo.vue
+++ components/xanzhu-header-logo.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="text-center sm:text-left mt-20 leading-5 font-display sm:ml-6 sm:leading-5 sm:mt-4 subpixel-antialiased">
+  <div class="text-center sm:text-left mt-10 leading-5 sm:ml-6 sm:leading-5 sm:mt-4 subpixel-antialiased">
     <n-link :to="{name: 'index'}" class="link" aria-label="Return Home">
-      <p class="version-date text-xs pb-1 font-medium tracking-wide">
+      <p class="version-date text-xs pb-1 font-bold tracking-wide hidden sm:flex">
         A1.1 — JULY 2020
       </p>
-      <h1 class="header-branding text-5xl font-bold">
+      <h1 class="text-5xl font-bold">
         XANZHU
       </h1>
     </n-link>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mt-20 text-center sm:mx-10">
-    <h3 class="font-medium text-2xl sm:text-3xl sm:leading-9">
+    <h2 class="font-medium text-2xl sm:text-3xl sm:leading-9">
       Our servers are currently being updated.
-    </h3>
+    </h2>
     <div class="mt-2 break-word text-sm leading-5 text-gray-300 sm:text-base">
       <p>
         This page will constantly update as we improve our design.
@@ -11,6 +11,6 @@
         We expect to release our website by the end of the year. Stay Tuned!
       </p>
     </div>
-    <img src="/favicon-DF42.png" class="pt-5 inline" aria-hidden="true">
+    <img src="/favicon-DF42.png" width="32" height="32" class="pt-5 inline" aria-hidden="true">
   </div>
 </template>
